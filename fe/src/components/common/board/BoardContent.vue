16a6f87f7217b4c4de64cb4cca7f21500fcd9337
BBaeGGom <dhtmdrl2004@naver.com>
2023-05-15 03:20:09 +0900

    feat : 게시판 사이드바 구현

---
+++ fe/src/components/common/board/BoardContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+    <div>content Test</div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
--- fe/src/views/BoardPage.vue
+++ fe/src/views/BoardPage.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col cols="4">
+          <board-side-bar></board-side-bar>
+        </b-col>
+        <b-col cols="8">
+          <board-content></board-content>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-export default {};
+import BoardSideBar from '../components/common/board/BoardSideBar.vue';
+import BoardContent from '../components/common/board/BoardContent.vue';
+export default {
+  components: {
+    BoardSideBar,
+    BoardContent,
+  },
+};
 </script>
 
 <style></style>
