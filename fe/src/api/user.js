1f5b072bc8658fa48156e3fe7c099c25906f4798
조명익 <auddlr100@naver.com>
2023-05-16 15:23:18 +0900

    feat : 사용자 정보 조회 axios 모듈 작성

--- fe/src/api/user.js
+++ fe/src/api/user.js
@@ -1,7 +1,11 @@
 import { users } from './index';
 
+function getUserInfo() {
+  return users.get();
+}
+
 function updateUser(data) {
   return users.put(`${data.id}`, data);
 }
 
-export { updateUser };
+export { getUserInfo, updateUser };
