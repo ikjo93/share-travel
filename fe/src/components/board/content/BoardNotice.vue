292bd921f9f079a317c3d802b89e9a18185b9108
BBaeGGom <dhtmdrl2004@naver.com>
2023-05-17 11:58:13 +0900

    feat : 게시판 내 카테고리별 vue 구현

---
+++ fe/src/components/board/content/BoardNotice.vue
@@ -0,0 +1,9 @@
+<template lang="">
+  <div>
+    <p>공지사항 페이지 입니다.</p>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
---
+++ fe/src/components/board/content/BoardTipForum.vue
@@ -0,0 +1,9 @@
+<template lang="">
+  <div>
+    <p>꿀팁게시판 페이지 입니다.</p>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
