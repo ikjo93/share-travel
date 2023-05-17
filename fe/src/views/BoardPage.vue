81d965627dd75baf79abc38a31ab1ae42d7a269f
BBaeGGom <dhtmdrl2004@naver.com>
2023-05-17 11:56:34 +0900

    feat : 게시판 점보트론 및 이동 버튼 구현

--- fe/src/views/BoardPage.vue
+++ fe/src/views/BoardPage.vue
@@ -1,23 +1,10 @@
 <template>
   <div>
     <b-container class="bv-example-row">
-      <b-jumbotron
-        header="커뮤니티"
-        lead="여러분만의 여행 팁을 공유해주세요 !"
-        bg-variant="white"
-      >
-        <!-- <img src="../../public/light.png" /> -->
-        <button>공지사항</button>
-        <button>이벤트</button>
-        <button>자유 게시판</button>
-        <button>꿀팁 게시판</button>
-      </b-jumbotron>
+      <board-jumbotron></board-jumbotron>
       <b-row>
-        <b-col cols="2" md="auto" :style="'background-color:blueviolet'">
-          <board-side-bar></board-side-bar>
-        </b-col>
-        <b-col cols="10" md="auto">
-          <board-content></board-content>
+        <b-col cols="12">
+          <router-view></router-view>
         </b-col>
       </b-row>
     </b-container>
@@ -25,22 +12,13 @@
 </template>
 
 <script>
-import BoardSideBar from '../components/common/board/BoardSideBar.vue';
-import BoardContent from '../components/common/board/BoardContent.vue';
+import BoardJumbotron from '../components/board/BoardJumbotron.vue';
 export default {
+  name: 'board',
   components: {
-    BoardSideBar,
-    BoardContent,
+    BoardJumbotron,
   },
 };
 </script>
 
-<style>
-button {
-  border-radius: 50px;
-  padding: 5px 12px;
-  overflow-x: auto;
-  margin: 5px;
-  white-space: nowrap;
-}
-</style>
+<style></style>
