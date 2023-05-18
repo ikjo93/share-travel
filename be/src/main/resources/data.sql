cdaa8fbe47ceaaf8a787f1abdf1312a7d19e359f
ikjo93 <auddlr100@naver.com>
2023-05-18 23:44:53 +0900

    feat : 테스트를 위한 DB SQL 파일 작성

---
+++ be/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO travel_keyword (name, created_date, modified_date)
+VALUES ('운동시설', NOW(), NOW()), ('야경', NOW(), NOW()), ('산책', NOW(), NOW()),
+       ('감수성', NOW(), NOW()), ('일몰', NOW(), NOW()), ('일출', NOW(), NOW());
