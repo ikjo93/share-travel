5f060799c479048d1d441af656b0fe2d431818f1
조명익 <auddlr100@naver.com>
2023-05-18 14:48:13 +0900

    feat : 여행지 키워드 도메인 기본 설계 (컨트롤러, 서비스, 엔티티, 리포지토리, DTO)

---
+++ be/src/main/java/com/sharetravel/domain/travelkeyword/entity/TravelKeyword.java
@@ -0,0 +1,20 @@
+package com.sharetravel.domain.travelkeyword.entity;
+
+import com.sharetravel.global.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "`travel_keyword`")
+@Entity
+public class TravelKeyword extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "travel_keyword_id")
+    private Long id;
+
+    private String name;
+}
