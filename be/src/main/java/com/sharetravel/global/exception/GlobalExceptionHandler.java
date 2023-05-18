e879247f21aa6953ffaf084b1f95a8b7cbeed343
조명익 <auddlr100@naver.com>
2023-05-18 09:56:03 +0900

    feat : WAS 글로벌 예외 처리 로직 작성

---
+++ be/src/main/java/com/sharetravel/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.sharetravel.global.exception;
+
+import com.sharetravel.global.ApiResponseCode;
+import com.sharetravel.global.ApiResponseMessage;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponseMessage> handleMethodArgumentTypeMismatchException() {
+        ApiResponseCode code = ApiResponseCode.SERVER_INTERNAL_ERROR;
+        return ResponseEntity.status(code.getHttpStatusCode())
+                .body(ApiResponseMessage.of(code.getCode(), code.getMessage()));
+    }
+}
