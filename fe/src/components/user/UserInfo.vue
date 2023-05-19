29977f72c2084394b7bf5824371b5c2cf5a54f78
조명익 <auddlr100@naver.com>
2023-05-19 15:57:08 +0900

    feat : 마이페이지 기본 컴포넌트 설계

---
+++ fe/src/components/user/UserInfo.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="profile-container">
+    <div class="profile-header">
+      <img
+        class="profile-picture"
+        :src="this.$store.state.user.picture"
+        alt="Profile Picture"
+      />
+    </div>
+    <div class="profile-info">
+      <h2>{{ this.$store.state.user.nickName }}</h2>
+      <p>{{ this.$store.state.user.email }}</p>
+    </div>
+    <div class="profile-modify-button">
+      <button @click="openProfileModal">수정</button>
+    </div>
+    <div class="keyword-container">
+      <h3>나의 여행지 키워드</h3>
+      <ul>
+        <li
+          v-for="keyword in this.$store.state.user.keywords"
+          :key="keyword.id"
+        >
+          {{ keyword.name }}
+        </li>
+      </ul>
+    </div>
+    <div class="keyword-modify-button">
+      <button @click="openKeywordModal">수정</button>
+    </div>
+    <div class="server-info">
+      <p>Logged in with: {{ this.$store.state.user.oauthProvider }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        profilePicture: 'path/to/profile-picture.jpg',
+        nickname: 'John Doe',
+        email: 'john.doe@example.com',
+        keywords: ['keyword1', 'keyword2', 'keyword3'],
+        server: 'Third-Party Server',
+      },
+    };
+  },
+  methods: {
+    openProfileModal() {
+      // Code to open the profile modification modal
+    },
+    openKeywordModal() {
+      // Code to open the keyword modification modal
+    },
+  },
+  mounted() {
+    const { keywords } = this.$store.state.user.keywords;
+    console.log(keywords[0]);
+  },
+};
+</script>
+
+<style>
+.profile-container {
+  text-align: center;
+}
+
+.profile-header {
+  margin-top: 20px;
+}
+
+.profile-picture {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+}
+
+.profile-info {
+  margin-top: 20px;
+}
+
+.profile-modify-button {
+  margin-top: 10px;
+}
+
+.keyword-container {
+  margin-top: 30px;
+}
+
+.keyword-container li {
+  list-style: none;
+}
+
+.keyword-modify-button {
+  margin-top: 10px;
+}
+
+.server-info {
+  margin-top: 30px;
+}
+</style>
