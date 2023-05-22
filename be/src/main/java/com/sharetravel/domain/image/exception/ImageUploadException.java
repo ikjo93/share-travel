78a757f2cb9a99852cf48ea45fdff825cb081ac6
조명익 <auddlr100@naver.com>
2023-05-22 18:10:15 +0900

    feat : AWS S3 이미지 업로드 기능 구현

---
+++ be/src/main/java/com/sharetravel/domain/image/exception/ImageUploadException.java
@@ -0,0 +1,8 @@
+package com.sharetravel.domain.image.exception;
+
+public class ImageUploadException extends RuntimeException {
+
+    public ImageUploadException(String message) {
+        super(message);
+    }
+}
